b2ee7065551f48461a2d35de316ddc70c719696d
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-10-26 16:07:33 +0700

    Add Variabel n dan i dengan tipe data int

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -1,1 +1,24 @@
 ﻿
+using System;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        int[] arr = new int[20];
+
+        int n;
+        int i;
+
+        public void input()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
